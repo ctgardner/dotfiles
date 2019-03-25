74e4a901b6b406694e679c4ea7b98bad48da7f21
ctgardner <ctgardner.australia@gmail.com>
2019-03-25 13:59:53 +1100

    Add asdf to install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bash_profile
