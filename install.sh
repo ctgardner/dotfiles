2611b22dd575755df916d097ebc339718bfa604f
ctgardner <ctgardner.australia@gmail.com>
2019-10-11 10:01:28 +1100

    Set hostname

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ./homebrew/install.sh
 ./screencapture.sh
 
+sudo scutil --set HostName CTGMBP
+
 # install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
